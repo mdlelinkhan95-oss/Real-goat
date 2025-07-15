f41bcc39f1b7a26d4ff034ee0c9d8ef348ea7d48
RAFI212240 <islamrak045@gmail.com>
2025-07-15 23:33:55 +0600

    Update 0dalle.js

--- scripts/cmds/0dalle.js
+++ scripts/cmds/0dalle.js
@@ -1,48 +1,34 @@
 const axios = require('axios');
-const fs = require('fs-extra');
-const path = require('path');
-
+const baseApiUrl = async () => {
+  const base = await axios.get(`https://raw.githubusercontent.com/Mostakim0978/D1PT0/refs/heads/main/baseApiUrl.json`);
+  return base.data.api;
+}; 
 module.exports = {
   config: {
-    name: "dalle3",
-    aliases: ["dalle"],
+    name: "dalle",
+    aliases: ["bing", "create", "imagine"],
     version: "1.0",
-    author: "JARiF",
+    author: "Dipto",
     countDown: 15,
     role: 0,
-    shortDescription: "Generate images by Dalle3",
-    longDescription: "Generate images by Unofficial Dalle3",
-    category: "download",
-    guide: {
-      en: "{pn} prompt"
-    }
-  },
-
-  onStart: async function ({ message, args }) {
-    try {
-      const k = args.join("");
-
-       const waitingMessage = await message.reply("Processing your request, please wait...");
-
-      const res = await axios.get(`https://www.annie-jarif.repl.co/dalle?prompt=${encodeURIComponent(k)}`);
-      const data = res.data.data.picUrlList;
-      const imgData = [];
-
-      for (let i = 0; i < data.length; i++) {
-        const imgResponse = await axios.get(data[i], { responseType: 'arraybuffer' });
-        const imgPath = path.join(__dirname, 'tmp', `${i + 1}.jpg`);
-        await fs.outputFile(imgPath, imgResponse.data);
-        imgData.push(fs.createReadStream(imgPath));
-      }
-
-      await message.reply({
-        attachment: imgData
-      });
-
-      await fs.remove(path.join(__dirname, 'tmp'));
+    description: "Generate images by Unofficial Dalle3",
+    category: "IMAGE GENERATOR",
+    guide: { en: "{pn} prompt" }
+  }, 
+  onStart: async({ api, event, args }) => {
+    const prompt = (event.messageReply?.body.split("dalle")[1] || args.join(" ")).trim();
+    if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with 'Dipto' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
+    try{
+const wait = api.sendMessage("Wait koro baby 😽", event.threadID);
+      const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008`);
+const imageUrls = response.data.imgUrls || [];
+      if (!imageUrls.length) return api.sendMessage("Empty response or no images generated.", event.threadID, event.messageID);
+      const images = await Promise.all(imageUrls.map(url => axios.get(url, { responseType: 'stream' }).then(res => res.data)));
+    api.unsendMessage(wait.messageID);
+   api.sendMessage({ body: `✅ | Here's Your Generated Photo 😘`, attachment: images }, event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
-      return message.reply(error.message);
+      api.sendMessage(`Generation failed!\nError: ${error.message}`, event.threadID, event.messageID);
     }
   }
-};
+                                                    }
