6e60a4afe928a142ce898b87e5356316e47f6e2c
RAFI212240 <islamrak045@gmail.com>
2025-07-21 19:30:52 +0600

    Update 0admin.js

--- scripts/cmds/0admin.js
+++ scripts/cmds/0admin.js
@@ -1,32 +1,30 @@
-module.exports.config = {
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const moment = require("moment-timezone");
+
+module.exports = {
+  config: {
     name: "admin",
     version: "1.0.0",
     permission: 0,
     credits: "nayan",
     prefix: true,
-    description: "",
+    description: "Shows admin info with photo",
     category: "prefix",
     usages: "",
     cooldowns: 5,
-    dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
-};
-module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-  
-var callback = () => api.sendMessage({body:`
+  },
+
+  onStart: async function({ api, event, message }) {
+    try {
+      const start = process.uptime();
+      const hours = Math.floor(start / 3600);
+      const minutes = Math.floor((start % 3600) / 60);
+      const seconds = Math.floor(start % 60);
+      const timeString = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+
+      const info = `
 --------------------------------------------
 𝐍𝐚𝐦𝐞        : 𝗔𝗹𝗶𝗳 𝗛𝗼𝘀𝘀𝗼𝗻
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝗔𝗹𝗶𝗳 𝗛𝗼𝘀𝘀𝗼𝗻
@@ -41,8 +39,36 @@ var callback = () => api.sendMessage({body:`
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801615623399
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/alifhosson
 𝐌𝐲 𝐁𝐢𝐨   : https://bio.link/alifhosson
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/Alifhosson.xxx`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100075421394195/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
-fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-   };
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/Alifhosson.xxx`;
+
+      // ফোল্ডার রেডি করো
+      const cacheDir = path.join(__dirname, "cache");
+      await fs.ensureDir(cacheDir);
+      const imgPath = path.join(cacheDir, "1.png");
+
+      // 1. Modern Facebook ID/Profile Image fetch, fallback if not found
+      const imgUrl = "https://graph.facebook.com/100075421394195/picture?height=720&width=720";
+      // const imgUrl = "https://pplx-res.cloudinary.com/image/upload/v1753004910/user_uploads/16172130/2c593b26-2174-4fd2-b713-7031e4cf5cc0/1000263194.jpg"; // fallback link
+
+      try {
+        const res = await axios.get(imgUrl, { responseType: "arraybuffer" });
+        await fs.writeFile(imgPath, Buffer.from(res.data, "binary"));
+      } catch (e) {
+        // fallback image দিন এখানে
+        await fs.copyFile("path/to/backup_local_admin_img.png", imgPath);
+      }
+
+      await message.reply({
+        body: info,
+        attachment: fs.createReadStream(imgPath)
+      });
+
+      setTimeout(() => fs.unlink(imgPath, () => {}), 5000);
+
+    } catch (err) {
+      console.error("Admin info CMD error:", err.message);
+      message.reply("❌ ইমেজ পাঠানো যাচ্ছে না, তথ্য ঠিক আছে।");
+    }
+  }
+};
+	      
